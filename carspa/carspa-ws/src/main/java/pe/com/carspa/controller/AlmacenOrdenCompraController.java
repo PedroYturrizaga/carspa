8f6e94f432d6025168f407bc4adeae4b9ff53231
Jordy Adolfo Vilchez Lecca <jvilchez@t-contacta.com>
2019-04-30 00:39:30 -0500

    descargando cambios

--- carspa/carspa-ws/src/main/java/pe/com/carspa/controller/AlmacenOrdenCompraController.java
+++ carspa/carspa-ws/src/main/java/pe/com/carspa/controller/AlmacenOrdenCompraController.java
@@ -2,23 +2,24 @@ package pe.com.carspa.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import pe.com.carspa.services.AlmacenOrdenCompraService;
 
 
 @RestController
 @RequestMapping("/almacenOrdenCompra")
 public class AlmacenOrdenCompraController {
 
-	private final Logger LOG = Logger.getLogger(AlmacenOrdenCompraController.class);
+	//private final Logger LOG = Logger.getLogger(AlmacenOrdenCompraController.class);
 
 	@Autowired
 	private HttpServletRequest servletRequest;
 	
 	
-	//@Autowired
-	//private AlmacenOrdenCompraService almacenOrdenCompraService;
+	@Autowired
+	private AlmacenOrdenCompraService almacenOrdenCompraService;
 
 }
